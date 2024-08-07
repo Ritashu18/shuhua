c573358980a67969235f0e2fcaa0936425fbe5bc
瑞塔 <115989056+Ritashu18@users.noreply.github.com>
2024-08-07 12:11:43 +0800

    Update load_page.js

--- js/load_page.js
+++ js/load_page.js
@@ -1,7 +1,7 @@
 
 export default function loadPage(page) {
   $('[data-page]').off('click')
-  $('#main').load(`https://ritashu18.github.io/shuhua/`, function () {
+  $('#main').load(`https://ritashu18.github.io/shuhua/${page}`, function () {
     $(window).scrollTop(0)
   })
 }

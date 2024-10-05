13115564b07976ed85a3ba7d90dc3ff7fe86912a
rita <liu@ritade-MacBook-Pro.local>
2024-10-05 22:26:18 +0800

    update load_page

--- js/load_page.js
+++ js/load_page.js
@@ -1,8 +1,10 @@
 
 export default function loadPage(page) {
   $('[data-page]').off('click')
-  $('#main').load(`https://ritashu18.github.io/shuhua/${page}.html`, function () {
-    $(window).scrollTop(0)
+  // $('#main').load(`https://ritashu18.github.io/shuhua/${page}.html`, function () {
+  $('#main').load(`./${page}.html`, function () {
+    
+  $(window).scrollTop(0)
   })
 }
 

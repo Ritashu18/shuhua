08be5c0994564e3459270bcda396996a5896bbe3
rita <liu@ritade-MacBook-Pro.local>
2024-10-17 13:47:47 +0800

    Resume revise

--- js/load_page.js
+++ js/load_page.js
@@ -1,8 +1,8 @@
 
 export default function loadPage(page) {
   $('[data-page]').off('click')
-  // $('#main').load(`https://ritashu18.github.io/shuhua/${page}.html`, function () {
-  $('#main').load(`./${page}.html`, function () {
+  $('#main').load(`https://ritashu18.github.io/shuhua/${page}.html`, function () {
+  // $('#main').load(`./${page}.html`, function () {
     
   $(window).scrollTop(0)
   })
